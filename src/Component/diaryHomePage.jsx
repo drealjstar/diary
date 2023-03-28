6a9eba5d174807a5e5fe8c51f5bfa573ce3d6afa
drealjstar <drealjstar@yahoo.com>
2023-03-28 16:39:50 -0700

    add a link

--- src/Component/diaryHomePage.jsx
+++ src/Component/diaryHomePage.jsx
@@ -10,16 +10,9 @@ const HomePage = () => {
             <div className="wrapper">
                 <div className="image"></div>
                 <div className="detail">
-                    <h2>Sign in</h2>
-                    <h3>User name</h3>
-                    <input type="text" className="input1" />
-                    <h3 className="password">Password</h3>
-                    <input type="password" className="input1" />
-                    <button className="signInButton">Sign in</button>
-                    <div className="option">
-                        <p>Or,</p>
-                        <Link to="/sign" className="sign">
-                            <p>Sign up</p>
+                    <div className="innerDetail">
+                        <Link to="/note" className="linkToNote">
+                            <p className="linkToNoteText">Click to Draft</p>
                         </Link>
                     </div>
                 </div>
